7570e3363b4b96ef626e973c179b64b9441cd344
hammock1 <farasylvain1977@hotmail.com>
2024-08-06 22:25:24 -0400

    Coursera Scrimba Wk1: Calculator

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-function errorMessage(){
+let num1 = 8;
+let num2 = 2
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+var para = document.getElementById("sum-el");
+function add(){
 	
-var para = document.getElementById("error");
-para.textContent = "Something went wrong, please try again";
+para.textContent =  (num1 + num2);	
+}
+
+function subtract(){
+	
+para.textContent =  (num1 - num2);
+	
+}
+
+function divide(){
+
+para.textContent =  (num1 / num2);
+}
+
+function multiply(){
+	
+para.textContent =  (num1 * num2);
 	
 	
 }
